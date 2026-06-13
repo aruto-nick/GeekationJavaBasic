b74cdc12b4ab0ae5770971fd6ebb93ef1100e026
aruto-nick <effo.conti329@gmail.com>
2026-06-13 20:09:43 +0900

    メソッド2を修正

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -10,17 +10,21 @@ public class GeekIntroduction {
 	}
 
 	public static void specialSkill(int num1, int num2){
+		if(num1<1 || num2<1){
+			System.out.println("num1とnum2は0より大きい値にしてください。");
+			return;
+		}
+		if(num2 <= num1){
+			System.out.println("num2はnum1より大きい値にしてください。");
+			return;
+		}
+		if (300 < num1 || 300 < num2) {
+			System.out.println("num1とnum2は300以下にしてください。");
+			return;
+		}
+		
 		for (int a =num1; num1<= a&& a<=  num2; a++){
-			if(num1<1 || num2<1){
-				System.out.println("num1とnum2は0より大きい値にしてください。");
-				return;
-			}else if(num2 <= num1){
-				System.out.println("num2はnum1より大きい値にしてください。");
-				return;
-			}else if (300 < num1 || 300 < num2) {
-				System.out.println("num1とnum2は300以下にしてください。");
-				return;
-			}else if (a % 3==0 && a % 5 == 0) {
+			if (a % 3==0 && a % 5 == 0) {
 				System.out.println(a+"は3の倍数かつ5の倍数です。");
 				continue;
 			}else if(a % 3 ==0) {
